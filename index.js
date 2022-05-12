cf9a8def7ebb285ef97c5723831fcd7579047f03
Bouhadjar Amine <bouhadjarmedamine@gmail.com>
2022-05-12 09:09:33 +0200

    Docker

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
-app.listen(8080, () => {
-    console.log('Server Listening')
+
+app.get('/', (req,res) => {
+    res.send("Welcome")
+})
+
+app.listen(3000, () => {
+    console.log('Server Listening !')
   })
