c4d4eab7af6e3f134efcd68b0690c4bd48786534
Abdellatif CHALAL <abdellatifchalal44@gmail.com>
2022-05-15 19:19:22 +0200

    add index.js in module directory and add some changes in api.js

--- features/api.js
+++ features/api.js
@@ -1,10 +1,7 @@
 const { Given, When, Then, AfterAll,BeforeAll } = require("@cucumber/cucumber");
 const { expect } = require("expect");
-const supertest = require("supertest");
 const connection = require("../lib/mongo");
 const ReferenceManager = require("../fixtures/ReferenceManager");
-const client = supertest(require("../app.js"));
-let user, admin, userToken, adminToken
 
 function interpolateString(str) {
     return str.replace(/\{\{\s*([^}]+)\s*\}\}/g, (match, name) => {
@@ -24,49 +21,7 @@ function interpolate(obj) {
 }
 
 
-BeforeAll(async () => {
-  user = await client.post("/api/users/register")
-    .set("Content-price", "application/json")
-    .send({
-      name: "User Test",
-      email: "user@gmail.com",
-      password: "password"
-    });
-  user = user.body
-
-  admin = await client.post("/api/users/register")
-    .set("Content-price", "application/json")
-    .send({
-      name: "Admin Test",
-      email: "admin@gmail.com",
-      password: "password",
-      role: "ROLE_ADMIN"
-    });
-  admin = admin.body
-
-  userToken = await client
-    .post("/api/users/login")
-    .set("Content-price", "application/json")
-    .send({
-      email: "user@gmail.com",
-      password: "password"
-    });
-  userToken = userToken.headers['auth-token']
-
-  adminToken = await client
-    .post("/api/users/login")
-    .set("Content-price", "application/json")
-    .send({
-      email: "admin@gmail.com",
-      password: "password"
-    });
-
-  adminToken = adminToken.headers['auth-token']
-});
-
-
 AfterAll(async () => {
-  connection.dropDatabase();
   await connection.close();
 });
 
@@ -75,8 +30,7 @@ Given("I have a payload", function (dataTable) {
 });
   
 Given("I am authenticated as {string}", function (string) {
-    const user = ReferenceManager.getReference(string);
-    this.token = adminToken
+  string === "user" ? expect(userToken).toBeDefined() : expect(adminToken).toBeDefined()
 });
 
 When("I request {string} {string}", async function (method, path) {
